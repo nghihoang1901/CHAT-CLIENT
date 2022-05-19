33a17ea75615976c3c2d72dc8d708def30eff698
Huu Nghi <hoanghuunghi113@gmail.com>
2022-05-19 19:37:42 +0700

    CHAT-CLIENT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
+import io from 'socket.io-client';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [socket, setSocket] = useState(null);
+  const [noidungchat, setNoiDungChat] = useState("");
+
+  useEffect(() => {
+    const newSocket = io(`http://${window.location.hostname}:4000`);
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, [setSocket]);
+
+  useEffect(() => {
+    if(socket !== null){
+      socket.on("respone_message_from_chat", (message) => {
+        console.log(message);
+      })
+    }
+  }, [socket])
+
+  const sendMessageToServer = (e) => {
+    e.preventDefault();
+    // let value = "test thu";
+    socket.emit("message_chat", noidungchat);
+    setNoiDungChat("");
+  }
+
+  const onChangeInput = (e) => {
+    setNoiDungChat(e.target.value);
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      { socket ? (
+        <div className="chat-container">
+          Socket is connected
+        </div>
+      ) : (
+        <div>Not Connected</div>
+      )}
+
+        
+        <form onSubmit={sendMessageToServer}>
+          <legend>Form chat</legend>
+        
+          <div class="form-group">
+            <label for="">nội dung</label>
+            <input value={noidungchat} type="text" class="form-control" id="" onChange={onChangeInput} />
+          </div>
+        
+          
+        
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        
+
     </div>
   );
 }
